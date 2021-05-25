39907a55b3e2f25038971a336d97230d60e6e9cf
vikas-4402 <68778119+vikas-4402@users.noreply.github.com>
2021-05-25 14:26:09 +0530

    First Commit
    Models are Added and Basic Views are added.

---
+++ shop/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=200,default = None)
+    price =models.IntegerField(blank=True ,null= True)
+    content = models.TextField(default = None)
+
+    def __str__(self):
+        return self.title
+
+class OrderItem(models.Model):
+    user =models.ForeignKey(User, on_delete = models.CASCADE)
+    item = models.ForeignKey(Item, on_delete =models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    quantity = models.IntegerField(default = 1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
+class Order(models.Model):
+    user =models.ForeignKey(User, on_delete = models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    ordered = models.BooleanField(default=False)
+    start_date =models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.user.username
---
+++ shop/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Item
+# Create your views here.
+def home(request):
+    context ={
+        'items' : Item.objects.all()
+    }
+    return render(request,'shop/home.html',context)
